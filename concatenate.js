93c62a10047b139543bcff851ab3eed1d4e09b39
testuser <testuser@example.com>
2020-11-12 17:07:03 +0530

    styles.js in concat

--- concatenate.js
+++ concatenate.js
@@ -9,12 +9,13 @@ fs.readdir('./dist/pos').then((files) => {
   for (let i = 0; i < files.length; i++) {
     if (files[i].indexOf('es5.js') != -1) es5Files.push('./dist/pos/' + files[i]);
     if (files[i].indexOf('es2015.js') != -1) es2015Filse.push('./dist/pos/' + files[i]);
-
-  };
+    es2015Filse.push('./dist/pos/styles.css');
+    es5Files.push('./dist/pos/styles.css');
+  }
 
   concatenate(es5Files, 'element-es5.js');
   concatenate(es2015Filse, 'element-es2015.js');
-})
+});
 
 async function concatenate(files, outFilename) {
   console.log(files)
